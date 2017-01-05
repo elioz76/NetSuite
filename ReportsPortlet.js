0217c57f6e3dc978aa97cae61f0566ebe01c3352
elioz76 <elioz76@gmail.com>
2017-01-05 13:03:54 -0500

    Add files via upload

---
+++ ReportsPortlet.js
@@ -0,0 +1,11 @@
+﻿function ReportsPortlet(portlet, column)
+{
+portlet.setTitle('Reports')
+var content = "<table border=0 cellpadding=3 cellspacing=0 width=100%>"+
+"<tr align=left ><td>Client Reports</td></tr>"+
+"<tr align=left ><td><a href='https://system.na1.netsuite.com/app/reporting/reportrunner.nl?cr=307&reload=t&whence='></a></td>"+
+"</table>";
+content = '<td><span>'+ content + '</span></td>'
+portlet.setHtml( content );
+
+}
